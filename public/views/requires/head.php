50d735d470ee89ee41d96a24496c8269ffab31ae
jarivankaam <jjj.vankaam@student.avans.nl>
2024-11-24 14:31:13 +0100

    ✨feature: small change

--- public/views/requires/head.php
+++ public/views/requires/head.php
@@ -15,5 +15,5 @@
     <link rel="stylesheet" href="public/css/main.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <title>Wat kan ik dragen vandaag?</title>
+    <title>Ioan is cute</title>
 </head>
