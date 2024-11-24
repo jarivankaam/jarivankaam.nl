2b4b9e7a6fa61912e2a8231850b6db9a06a15d55
jarivankaam <jjj.vankaam@student.avans.nl>
2024-11-24 23:47:49 +0100

    ✨feature: added projects and basic layout

--- public/views/index.php
+++ public/views/index.php
@@ -1,9 +1,91 @@
 <html lang="en">
 <?php require_once 'requires/head.php'; ?>
-<?php require_once 'requires/header.php'; ?> 
-
+<?php require_once 'requires/header.php'; ?>
 
 <body>
+
+  <section class="section-hero-image">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-1 2 col-md-6 col-lg-6">
+          <img src="https://i.imgur.com/34uE87Y.png" alt="Jari van kaam">
+          <h2>Informatica student <br>front-end developer</h2>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+          <h2>Fast results, Deep research</h2>
+          <p>I'am Jari van kaam a informatica student and a front-end developer with 1 year of experience in the field.</p>
+          <p>I create designs, backend-solutions and beatifull front-end creations based on the needs of the customer..</p>
+          <p>Having worked as a full-stack developer i got enough knowledge to work on developer level even while still being a student.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section-projects">
+    <div class="container">
+      <div class="row">
+        
+        <div class="modal-project">
+          <div class="title">
+          <h2>Projects</h2>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+            <div class="card-wrapper flex flex-column">
+              <div class="title-wrapper">
+                <h2>Jari van kaam.nl</h2>
+                <p>Wordpress</p>
+              </div>
+              <img src="https://i.imgur.com/e2cJ78y.png" alt="Jari van kaam.nl Wordpress">
+              <div class="button-wrapper flex align-items-center justify-content-center"><button>Read more</button></div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+            <div class="card-wrapper flex flex-column">
+            <div class="title-wrapper">
+                <h2>Jari van kaam.nl</h2>
+                <p>Wordpress</p>
+              </div>
+              <img src="https://i.imgur.com/e2cJ78y.png" alt="Jari van kaam.nl Wordpress">
+              <div class="button-wrapper flex align-items-center justify-content-center"><button>Read more</button></div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+            <div class="card-wrapper flex flex-column">
+            <div class="title-wrapper">
+                <h2>Jari van kaam.nl</h2>
+                <p>Wordpress</p>
+              </div>
+              <img src="https://i.imgur.com/e2cJ78y.png" alt="Jari van kaam.nl Wordpress">
+              <div class="button-wrapper flex align-items-center justify-content-center"><button>Read more</button></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+  </section>
+  <section class="section-contanct">
+    <div class="container">
+    <div >
+          <div>
+             <h2>Contact</h2>
+          <p>Email: jari@example.com</p>
+          <p>Phone: +1234567890</p>
+          <button>Get in touch</button>
+        </div>
+    </div>
+  </section>
+  <section class="section-skills">
+    <div class="container">
+      <h2>Skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
+  </section>
 </body>
 <script>
   AOS.init();
